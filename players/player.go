b233df6aa761dd215d99764f6e8a49a8c76023c3
Tuix GmbH <development@tuix.ch>
2024-02-16 17:13:04 +0100

    v0.0.5

--- players/player.go
+++ players/player.go
@@ -1,7 +1,7 @@
 package players
 
 // Player is a struct that represents a player
-// Name attribute is the Player's name blah!
+// Name attribute is the Player's name blah 111!
 type Player struct {
 	Name string
 }
